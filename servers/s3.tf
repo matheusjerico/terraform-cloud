0cc28d7e85d48a3e263044a8aa2fb8bc15146c6f
Matheus Jericó Palhares <matheus.palhares@luizalabs.com>
2021-05-16 16:33:58 -0300

    deleting files

--- servers/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  tags = var.bucket_tags
-}
--- terrafile.tf
+++
@@ -1,9 +0,0 @@
-module "servers" {
-  source = "./servers"
-
-  instance_type = var.instance_type
-  ec2_tags      = var.ec2_tags
-  bucket_name   = var.bucket_name
-  bucket_tags   = var.bucket_tags
-  blocks_device = var.blocks_device
-}
